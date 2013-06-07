8e834cc3554ef009cab1426c18d68758f0d7b953
supster <supatra.indhara@gmail.com>
2013-06-06 20:35:37 -0400

    review index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,4 +17,14 @@ class ReviewsController < ApplicationController
       render "new"
     end  	
   end
+
+  def index
+  	@program = Program.find(params[:prog])
+  	if params[:loc].nil? 
+  		@reviews = @program.reviews 		
+  	else
+  		@location = Location.find(params[:loc]) 
+  		@reviews = @program.agency.locations.find_by_id(params[:loc]).reviews
+  	end
+  end
 end
